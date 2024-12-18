ffaad343201bd28df6498815cb1bc25ee0dfa8c9
SIDDHESH SURYAKANT PAWAR <siddheshpawar785@gmail.com>
2024-12-18 18:46:07 +0530

    Update Assignment2StudentData.cpp

--- Assignment2StudentData.cpp
+++ Assignment2StudentData.cpp
@@ -1,3 +1,11 @@
+// Develop an object oriented program in C++ to create a database of student information 
+// system containing the following information: Name, Roll number, Class, division, Date of 
+// Birth, Blood group, Contact address, telephone number, driving license no. and other. 
+// Construct the database with suitable member functions for initializing and destroying the 
+// data viz constructor, default constructor, Copy constructor, destructor, static member 
+// functions, friend class, this pointer, inline code and dynamic memory allocation 
+// operators-new and delete. 
+
 #include<iostream>
 #include<string.h>
 using namespace std;
